4e8e843673e26865e3cf4f094bf29c519efbe8a4
Victoria Sales <contatovictoriasales@gmail.com>
2023-05-11 14:18:49 -0300

    Requisito 06 OK

---
+++ challenges/desafio6.sql
@@ -0,0 +1,8 @@
+SELECT 
+  MIN(p.valor) AS faturamento_minimo,
+  MAX(p.valor) AS faturamento_maximo,
+  ROUND(AVG(p.valor), 2) AS faturamento_medio,
+  SUM(p.valor) AS faturamento_total
+FROM planos AS p
+INNER JOIN usuarios AS u
+ON u.id_plano = p.id;
